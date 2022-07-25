afe1b41ff440f2fd93d3f251e8aabc3af8f2b224
Vishwajeet <singhrathorev21@gmail.com>
2022-07-25 19:36:44 +0530

    editable room about added

--- src/components/EditableInput.js
+++ src/components/EditableInput.js
@@ -7,6 +7,7 @@ const EditableInput = ({
   label = null,
   placeholder = 'Write your Value',
   emptyMsg = 'Input is empty',
+  wrapperClassName = '',
   ...inputProps
 }) => {
   const [input, setInput] = useState(initialValue);
@@ -30,7 +31,7 @@ const EditableInput = ({
   };
 
   return (
-    <div>
+    <div className={wrapperClassName}>
       {label}
       <InputGroup>
         <Input
