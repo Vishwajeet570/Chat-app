099b950585dd9375d2b0c9bfd3b8239cbcedd5e3
Vishwajeet <singhrathorev21@gmail.com>
2022-07-26 17:50:09 +0530

    grouped messages

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -40,3 +40,13 @@ export async function getUserUpdates(userId, keyToUpdate, value, db) {
   });
   return updates;
 }
+export function groupBy(array, groupingKeyfn) {
+  return array.reduce((result, item) => {
+    const groupingKey = groupingKeyfn(item);
+    if (!result[groupingKey]) {
+      result[groupingKey] = [];
+    }
+    result[groupingKey].push(item);
+    return result;
+  }, {});
+}
